4820798eadffc538c9ccce1309066ac964832c66
hoshi_umeko <hoshi.kakiko@gmail.com>
2019-04-27 17:02:13 +0900

    タイトル画面にスタートボタン追加

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+
+    //ボタンクリックでMainGameへ
+    public void ButtonClicked()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
