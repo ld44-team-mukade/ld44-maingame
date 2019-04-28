d3e53015c21845c06ad57d22dca2b6d7c2668bd9
ray45422 <ray45422@gmail.com>
2019-04-28 22:54:01 +0900

    回転軸の設定と弾の大きさを変更できるようにした
    モデルが来たらもうちょっと詰める

--- Assets/Project/Prefabs/Weapon/CannonController.cs
+++ Assets/Project/Prefabs/Weapon/CannonController.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class CannonController : MonoBehaviour
 {
-    // 射出するもの
+    // 発射するもののサイズ
+    public Vector3 bulletSize = new Vector3(1, 1, 1);
+    // 発射するもの
     public Rigidbody bullet;
     // 銃身になるオブジェクト
     public GameObject barrel;
+    // 旋回軸
+    public GameObject pivot;
+    // 上下軸
+    public GameObject pitch;
     // 弾の初速
     public int initSpeed = 100;
     // 狙う相手(まだ何も使ってない)
@@ -21,20 +27,39 @@ public class CannonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A))
+    }
+
+    private void Test()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
         {
             Fire();
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            pivot.transform.Rotate(0, 1f, 0);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            pivot.transform.Rotate(0, -1f, 0);
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            pitch.transform.Rotate(-1f, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            pitch.transform.Rotate(1f, 0, 0);
         }
     }
 
     public Rigidbody Fire()
     {
-        Vector3 offset = transform.up;
-        // Y軸に沿って射出する
-        // 軸を変えるなら書き換えて
-        offset  *= barrel.transform.localScale.y + this.bullet.transform.localScale.y;
+        Vector3 offset = barrel.transform.up;
+        offset  *= barrel.transform.lossyScale[1] + this.bullet.transform.lossyScale[1];
 
         Rigidbody bullet = Instantiate(this.bullet, barrel.transform.position + offset, barrel.transform.rotation);
+        bullet.transform.localScale = this.bulletSize;
         Vector3 velocity = barrel.transform.up * initSpeed;
         bullet.velocity = velocity;
         return bullet;
