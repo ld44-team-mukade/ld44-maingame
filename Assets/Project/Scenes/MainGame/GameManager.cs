8f86dbb40d8ef3185e829f78ff1f9151036fd2d3
hoshi_umeko <hoshi.kakiko@gmail.com>
2019-04-28 15:30:47 +0900

    Playerが破壊された際にGameOver画面へ遷移

--- Assets/Project/Scenes/MainGame/GameManager.cs
+++ Assets/Project/Scenes/MainGame/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -24,6 +25,6 @@ public class GameManager : MonoBehaviour
     }
 
     void GoToGameOver(){
-        Debug.Log("GoToGameOver");
+        SceneManager.LoadScene("GameOver");
     }
 }
