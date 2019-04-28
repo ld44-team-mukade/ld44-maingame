98d0dab0f2cb70123aa9fc529856e94782e1147c
hoshi_umeko <hoshi.kakiko@gmail.com>
2019-04-28 20:15:58 +0900

    燃料タンクアイテムのアニメーションと爆発エフェクト追加

---
+++ Assets/Project/Prefabs/FuelTank/FuelTankItemAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelTankItemAnimation : MonoBehaviour
+{
+
+    //アイテムの移動範囲
+    public int range;
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sin = Mathf.Sin(speed * Mathf.PI * Time.time);
+
+        transform.position = new Vector3(transform.position.x, sin * range, transform.position.z);
+    }
+
+}
