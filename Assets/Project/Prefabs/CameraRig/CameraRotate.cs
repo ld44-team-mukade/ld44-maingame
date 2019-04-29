b72d7235c4dcf317f1026df09739fbbaf515e8cc
hoshi_umeko <hoshi.kakiko@gmail.com>
2019-04-29 18:42:33 +0900

    カメラの拡大縮小、Player視点移動(y軸のみ)を追加

---
+++ Assets/Project/Prefabs/CameraRig/CameraRotate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void CameraMove(Transform target,float _rotate)
+    {
+
+        float mouseInputX = Input.GetAxis("Mouse X");
+        float mouseInputY = Input.GetAxis("Mouse Y");
+
+        if (Input.GetMouseButton(1))
+        {
+            transform.RotateAround(target.transform.position, Vector3.up, _rotate * mouseInputX);
+            //transform.RotateAround(target.transform.position, transform.right, _rotate * mouseInputY);
+
+        }
+
+    }
+
+}
