2cdf4009eb0128501cb30d8a84a80faa3af1fe63
tanitta <ttata.trit@gmail.com>
2019-04-29 22:21:10 +0900

    Enable to work cannon.

--- Assets/Project/Prefabs/Weapon/CannonController.cs
+++ Assets/Project/Prefabs/Weapon/CannonController.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class CannonController : MonoBehaviour
 {
-    // 発射するもののサイズ
-    public Vector3 bulletSize = new Vector3(1, 1, 1);
     // 発射するもの
-    public Rigidbody bullet;
-    // 銃身になるオブジェクト
-    public GameObject barrel;
-    // 旋回軸
-    public GameObject pivot;
-    // 上下軸
-    public GameObject pitch;
+    public Rigidbody bulletPrefab;
+
     // 弾の初速
-    public int initSpeed = 100;
+    public float initSpeed = 100f;
+
     // 狙う相手(まだ何も使ってない)
-    protected Transform target;
+    [SerializeField]
+    private Transform _target;
+
+    [SerializeField]
+    private Transform _turret;
+
+    [SerializeField]
+    private Transform _barrel;
+
+    [SerializeField]
+    private Transform _muzzle;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +31,9 @@ public class CannonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var targetDirection = _target.position - _turret.transform.position;
+        RotateTurret(targetDirection);
+        Test();
     }
 
     private void Test()
@@ -35,38 +42,27 @@ public class CannonController : MonoBehaviour
         {
             Fire();
         }
-        if(Input.GetKey(KeyCode.RightArrow))
-        {
-            pivot.transform.Rotate(0, 1f, 0);
-        }
-        if(Input.GetKey(KeyCode.LeftArrow))
-        {
-            pivot.transform.Rotate(0, -1f, 0);
-        }
-        if(Input.GetKey(KeyCode.UpArrow))
-        {
-            pitch.transform.Rotate(-1f, 0, 0);
-        }
-        if(Input.GetKey(KeyCode.DownArrow))
-        {
-            pitch.transform.Rotate(1f, 0, 0);
-        }
     }
 
     public Rigidbody Fire()
     {
-        Vector3 offset = barrel.transform.up;
-        offset  *= barrel.transform.lossyScale[1] + this.bullet.transform.lossyScale[1];
-
-        Rigidbody bullet = Instantiate(this.bullet, barrel.transform.position + offset, barrel.transform.rotation);
-        bullet.transform.localScale = this.bulletSize;
-        Vector3 velocity = barrel.transform.up * initSpeed;
-        bullet.velocity = velocity;
-        return bullet;
+        var bulletInstance = Instantiate(bulletPrefab, _muzzle.transform) as Rigidbody;
+        bulletInstance.velocity = _muzzle.transform.forward * initSpeed;
+        bulletInstance.transform.parent = null;
+        return bulletInstance;
     }
 
-    public void SetTarget(Transform target)
-    {
-        this.target = target;
+    void RotateTurret(Vector3 targetDirection){
+        var localDirection = transform.InverseTransformDirection(targetDirection);
+        Vector3 localDirectionOnXZ = localDirection; localDirectionOnXZ.y = 0;
+        var qY = Quaternion.LookRotation (localDirectionOnXZ);
+        _turret.localRotation = qY;
+        Vector3 localDirectionOnWY = new Vector3(0, localDirection.y, localDirectionOnXZ.magnitude);
+        var qX = Quaternion.LookRotation (localDirectionOnWY);
+        _barrel.localRotation = qX;
+    }
+
+    public Vector3 BulletCenter(){
+        return _turret.position;
     }
 }
