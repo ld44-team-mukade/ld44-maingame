d2e609bc4caf55b3d4d2094d0532fe0fd561f3b3
hoshi_umeko <hoshi.kakiko@gmail.com>
2019-04-29 13:12:53 +0900

    砲弾、船の燃料回りの処理をもろもろ追加

--- Assets/Project/Prefabs/FuelTankItem/ItemAnime.cs
+++ Assets/Project/Prefabs/FuelTankItem/ItemAnime.cs
@@ -2,25 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FuelTankItemAnimation : MonoBehaviour
+public class ItemAnime : MonoBehaviour
 {
 
     //アイテムの移動範囲
     public int range;
     public int speed;
+    float sin1 = 0;
+    float sin2 = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+ 
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float sin = Mathf.Sin(speed * Mathf.PI * Time.time);
+        sin1 = Mathf.Sin(speed * Mathf.PI * Time.time);        
 
-        transform.position = new Vector3(transform.position.x, sin * range, transform.position.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y+(sin1 * range) - sin2, transform.position.z);
+
+        sin2 = sin1 * range;
     }
 
 }
