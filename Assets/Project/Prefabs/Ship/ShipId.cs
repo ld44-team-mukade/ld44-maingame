232ca81221a520164e98f669322a467e3de18d67
tanitta <ttata.trit@gmail.com>
2019-04-29 15:27:36 +0900

    Enable to register shipid to GameManagemenr.

---
+++ Assets/Project/Prefabs/Ship/ShipId.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipId : MonoBehaviour
+{
+    [HideInInspector]
+    public int Id = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Project/Scenes/MainGame/GameManager.cs
+++ Assets/Project/Scenes/MainGame/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,20 +8,51 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private Transform _playerShip;
+
+    [SerializeField]
+    private Transform shipPool;
+    private Dictionary<int, ShipId> _shipIdDict;
+
     [SerializeField]
     private List<Transform> _planes;
     private List<Transform> _sponer;
     // Start is called before the first frame update
+    private int _idCounter = 0;
+
+    void Awake(){
+        _shipIdDict = new Dictionary<int, ShipId>();
+    }
     void Start()
     {
-        
+        _idCounter = 0;
+        var shipIds = shipPool.GetComponentsInChildren<ShipId>().ToList();
+        foreach (var shipId in shipIds)
+        {
+            shipId.Id = _idCounter;
+            _shipIdDict[_idCounter] = shipId;
+            _idCounter++;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateShipDict();
+        foreach (var key in _shipIdDict)
+        {
+            Debug.Log(key);
+        }
+
         if(!_playerShip){
             GoToGameOver();
+        }
+    }
+
+    void UpdateShipDict(){
+        var nullElemKeys = _shipIdDict.Keys.Select((key) => _shipIdDict[key]?-1:key).Where(key => key != -1).ToList();
+        foreach (var nullelemKey in nullElemKeys)
+        {
+            _shipIdDict.Remove(nullelemKey);
         }
     }
 
