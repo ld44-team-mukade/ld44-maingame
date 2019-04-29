af57d3a497e053ff7eff623f0585e3b8a9246a56
hoshi_umeko <hoshi.kakiko@gmail.com>
2019-04-29 14:05:28 +0900

    灯台のライトくるくるできようにした

---
+++ Assets/Project/Prefabs/LightHouse/LightHouseController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightHouseController : MonoBehaviour
+{
+
+    public float rotate;
+    public GameObject lightObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        lightObj.transform.Rotate(new Vector3(0f, rotate, 0f) * Time.deltaTime);
+
+    }
+}
