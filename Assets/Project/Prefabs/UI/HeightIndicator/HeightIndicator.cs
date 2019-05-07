9912b9766c42fb8f502f3ed41e08dcc0112dccf1
hoshi_umeko <hoshi.kakiko@gmail.com>
2019-05-08 01:08:21 +0900

    階層表示のUIにて、自機がいる階層のリングを大きくした

--- Assets/Project/Prefabs/UI/HeightIndicator/HeightIndicator.cs
+++ Assets/Project/Prefabs/UI/HeightIndicator/HeightIndicator.cs
@@ -14,16 +14,24 @@ public class HeightIndicator : MonoBehaviour
 
     [SerializeField]
     private Material blue;
+    
+    [SerializeField]
+    private float _scaleSize = 1.2f;
 
     [SerializeField]
     private List<Renderer> _ringRenderers;
 
+    private Vector3 _ringScales;
+
+    private Vector3 _startValue;
 
     void Start()
     {
         // _ringRenderers = GetComponentsInChildren<Renderer>().ToList();
         // _ringRenderers.RemoveAt(0);
-    }
+        _startValue = _ringRenderers[1].transform.localScale;
+        _ringScales = new Vector3(_scaleSize, 1f, _scaleSize);
+}
 
     // Update is called once per frame
     void Update()
@@ -33,7 +41,10 @@ public class HeightIndicator : MonoBehaviour
         // shipController.currentHeightIndex;
         foreach(var renderer in _ringRenderers){
             renderer.material = white;
+            renderer.transform.localScale = _startValue;
         }
+
+        _ringRenderers[shipController.currentHeightIndex].transform.localScale = Vector3.Scale(_ringRenderers[shipController.currentHeightIndex].transform.localScale, _ringScales);
         _ringRenderers[shipController.currentHeightIndex].material = blue;
     }
 }
