2d83bd41e01a90afdfb1292cea892dc5293c0a4f
tanitta <ttata.trit@gmail.com>
2019-05-12 14:25:38 +0900

    Enable to turn off ship light when fuel is empty. #230

---
+++ Assets/Project/Prefabs/Ship/LightController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    List<Light> _lights;
+    FuelTank _fuelTank;
+
+    void Awake(){
+        _fuelTank = GetComponent<FuelTank>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lights = GetComponentsInChildren<Light>().ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(0f < _fuelTank.Remaining()) return;
+        foreach (var light in _lights)
+        {
+            light.enabled = false;
+        }
+    }
+}
