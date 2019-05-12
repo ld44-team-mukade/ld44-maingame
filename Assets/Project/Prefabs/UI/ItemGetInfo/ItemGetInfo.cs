c972e0717f3c2298ce540a11d2b133a886220e2f
hoshi_umeko <hoshi.kakiko@gmail.com>
2019-05-12 23:27:31 +0900

    テスト

---
+++ Assets/Project/Prefabs/UI/ItemGetInfo/ItemGetInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGetInfo : MonoBehaviour
+{
+    [SerializeField]
+    TextMesh _FuelIncText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.parent = Camera.main.transform;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ItemGetAnime(float fuelamount)
+    {
+        _FuelIncText.text = "+" + fuelamount.ToString();
+
+    }
+
+}
